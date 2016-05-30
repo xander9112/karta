3f2458f3337eb37bd393873dd100062a20d715f7
AlexZander <xander91@mail.ru>
2016-05-30 15:26:35 +0300

    Добавлены файлы.

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const rimraf = require('rimraf');
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
+const gulpsync = require('gulp-sync')(gulp);
+const config = require('./gulp/configs/main.config');
+const register = require(`./gulp/utils/register`);
+
+register(gulp, plugins, config)([
+    'build-vendor',
+    'build-js',
+    'build-sprite',
+    'build-styles',
+    'build-styles-blocks',
+    'copy',
+    'serve',
+    'watch',
+    'deploy'
+]);
+
+gulp.task('clean', function (callback) {
+    return rimraf(config.clean, callback);
+});
+
+// gulp.task('build', gulp.parallel('build-vendor', 'build-js', 'build-sprite', 'build-styles-blocks', 'build-styles', 'copy'));
+gulp.task('build', gulp.parallel('build-vendor', 'build-js', 'build-sprite', 'build-styles-blocks', 'build-styles', 'copy'));
+gulp.task('default', gulp.series('build', gulp.parallel('watch', 'serve')));
